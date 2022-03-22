014789bec60ca86d2f048f9f4c96bd4eaa5f8eab
Zhengyang Liu <liuz@cs.utah.edu>
2022-03-22 15:58:22 -0700

    statistics

--- lib/Utils.cpp
+++ lib/Utils.cpp
@@ -42,7 +42,7 @@ hGet(const char* s, unsigned sz, std::string &Value, redisContext *c) {
 
 void
 hSet(const char* s, unsigned sz, llvm::StringRef Value, redisContext *c, unsigned oldcost, unsigned newcost) {
-  redisReply *reply = (redisReply *)redisCommand(c, "HSET %b rewrite %s %s %s",
+  redisReply *reply = (redisReply *)redisCommand(c, "HSET %b rewrite %s oldcost %s newcost %s",
     s, sz, Value.data(), std::to_string(oldcost).c_str(), std::to_string(newcost).c_str());
   if (!reply || c->err)
     llvm::report_fatal_error((llvm::StringRef)"Redis error: " + c->errstr);
