7c03d2e9bde99715c4660603a548880257bb1391
John Regehr <regehr@cs.utah.edu>
2022-09-14 06:48:08 +0100

    add a quick llvm pass for figuring out size of function arguments (#10)

---
+++ analysis_pass_for_paper/HelloWorld.cpp
@@ -0,0 +1,85 @@
+//=============================================================================
+// FILE:
+//    HelloWorld.cpp
+//
+// DESCRIPTION:
+//    Visits all functions in a module, prints their names and the number of
+//    arguments via stderr. Strictly speaking, this is an analysis pass (i.e.
+//    the functions are not modified). However, in order to keep things simple
+//    there's no 'print' method here (every analysis pass should implement it).
+//
+// USAGE:
+//    2. New PM
+//      opt -load-pass-plugin=libHelloWorld.dylib -passes="hello-world" `\`
+//        -disable-output <input-llvm-file>
+//
+//
+// License: MIT
+//=============================================================================
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+//-----------------------------------------------------------------------------
+// HelloWorld implementation
+//-----------------------------------------------------------------------------
+// No need to expose the internals of the pass to the outside world - keep
+// everything in an anonymous namespace.
+namespace {
+
+// This method implements what the pass does
+void visitor(Function &F) {
+  errs() << F.getName() << " : ";
+  auto DL = F.getParent()->getDataLayout();
+  long size = 0;
+  for (auto &A :  F.args()) {
+    size += DL.getTypeSizeInBits(A.getType());
+  }
+  errs() << size << "\n";
+}
+
+// New PM implementation
+struct HelloWorld : PassInfoMixin<HelloWorld> {
+  // Main entry point, takes IR unit to run the pass on (&F) and the
+  // corresponding pass manager (to be queried if need be)
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+    visitor(F);
+    return PreservedAnalyses::all();
+  }
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired() { return true; }
+};
+
+} // namespace
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "hello-world") {
+                    FPM.addPass(HelloWorld());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize HelloWorld when added to the pass pipeline on the
+// command line, i.e. via '-passes=hello-world'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getHelloWorldPluginInfo();
+}
+
