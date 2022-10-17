2c269c02ef2e4e739a08fa38417859f7b6b11491
Zhengyang Liu <liuz@cs.utah.edu>
2022-10-17 17:56:12 +0000

    foo

--- lib/EnumerativeSynthesis.cpp
+++ lib/EnumerativeSynthesis.cpp
@@ -670,7 +670,7 @@ push:
         F.dump();
       }
       if (config::ignore_machine_cost ||
-          !machinecost || !newcost || newcost <= machinecost) {
+          !machinecost || !newcost || newcost < machinecost) {
         if (config::debug_enumerator) {
           llvm::errs()<<"=== successfully synthesized rhs ===\n";
         }
