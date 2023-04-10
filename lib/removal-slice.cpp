50db1a7c360013e11d32301abf2a1989f93cf111
Zhengyang Liu <liuz@cs.utah.edu>
2023-04-10 04:44:58 +0000

    foo

--- lib/removal-slice.cpp
+++ lib/removal-slice.cpp
@@ -4,6 +4,7 @@
 #include "config.h"
 #include "removal-slice.h"
 
+#include "llvm/ADT/SmallSet.h"
 #include "llvm/IR/Function.h"
 
 #include <optional>
@@ -13,10 +14,20 @@ using namespace std;
 
 namespace minotaur {
 
+static void remove_unreachable() {
+
+}
+
 optional<reference_wrapper<Function>> RemovalSlice::extractExpr(Value &v) {
   if(config::debug_slicer) {
     llvm::errs() << ">>> slicing value " << v << ">>>\n";
   }
+
+
+  SmallSet<Value*, 16> Harvested;
+
+
+
   SmallVector<Type *, 4> argTys;
   Function *F = Function::Create(FunctionType::get(v.getType(), argTys, false),
                                  GlobalValue::ExternalLinkage, "rewrite", *m);
